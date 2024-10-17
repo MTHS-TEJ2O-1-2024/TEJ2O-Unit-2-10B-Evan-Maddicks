b2ca21791db1a8eccbcde42f644767837067bb1a
Evan-Maddicks <evan.maddicks@mths.ca>
2024-10-17 13:14:05 -0400

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,47 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Evan M
+ * Created on: Oct 2024
+ * This program will read the light levels in a roon
 */
 
-basic.showString('Hello, World!')
+let lightLevel = 0
+let neopixelStrip: neopixel.Strip = null
+neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+
+basic.clearScreen()
+neopixelStrip.clear()
+neopixelStrip.show()
+basic.showIcon(IconNames.Happy)
+
+basic.forever(function () {
+    lightLevel = input.lightLevel()
+    neopixelStrip.clear()
+    if (lightLevel <= 51) {
+        // Light up 0 NeoPixels
+    }
+    if (lightLevel > 52 && lightLevel <= 104) {
+        // Light up 1 NeoPixel
+        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+    }
+    if (lightLevel > 104 && lightLevel <= 156) {
+        // Light up 2 NeoPixels
+        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+    }
+    if (lightLevel > 156 && lightLevel <= 208) {
+        // Light up 3 NeoPixels
+        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+    }
+    if (lightLevel > 208) {
+        // Light up 4 NeoPixels
+        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+        neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Red))
+    }
+    neopixelStrip.show()
+    basic.pause(1000)
+})
